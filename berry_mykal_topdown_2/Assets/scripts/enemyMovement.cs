4bd1093a4ac43bbd70bd0e13d4c7bc3df0b54968
mykal716 <mykalberry716@gmail.com>
2021-05-11 20:16:34 -0700

    Initial Project Upload

---
+++ berry_mykal_topdown_2/Assets/scripts/enemyMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    public float detectionRadius = 3;
+    public float movementSpeed = 5;
+    public bool canMove = false;
+    public bool movementDirection = false;
+    public bool isFollowing = false;
+
+    private Animator myAnimator;
+
+    public Transform playerTarget;
+    private Rigidbody2D myRB;
+    private CircleCollider2D detectionZone;
+    private Vector2 up;
+    private Vector2 down;
+    private Vector2 zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = new Vector2(0, movementSpeed);
+        down = new Vector2(0, -movementSpeed);
+        zero = new Vector2(0, 0);
+
+        playerTarget = GameObject.Find("playerSprite").transform;
+
+        myRB = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        detectionZone = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        detectionZone.radius = detectionRadius;
+
+        if (isFollowing == false)
+        {
+            myAnimator.SetBool("isWalking", false);
+            myRB.velocity = zero;
+        }
+
+        else if (isFollowing == true)
+        {
+            Vector3 lookPos = playerTarget.position - transform.position;
+            float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+            myRB.rotation = angle;
+            lookPos.Normalize();
+
+            myAnimator.SetBool("isWalking", true);
+
+            myRB.MovePosition(transform.position + (lookPos * movementSpeed * Time.deltaTime));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("bullet"))
+        {
+            Debug.Log("I've been hit");
+            Destroy(collision.gameObject);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerCollision2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("player"))
+        {
+            isFollowing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("player"))
+        {
+            isFollowing = false;
+        }
+    }
+}
---
+++ berry_mykal_topdown_2/Assets/scripts/playerController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public GameObject bullet;
+    private Rigidbody2D myRB;
+    public Sprite invincibility;
+
+    private AudioSource mySpeaker;
+    public AudioClip pickup;
+    public AudioClip death;
+    public AudioClip shoot;
+
+    private bool canShoot = true;
+    public float shootCooldownTime;
+    private float timeDifference;
+    public bool invincible = false;
+    public float invincibleCooldownTime;
+    private float timeDifference2;
+
+    public float speed = 10;
+    public float bulletLifespan = 1;
+    public float bulletSpeed = 15;
+    public int playerHealth = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        mySpeaker = GetComponent<AudioSource>();
+        playerHealth = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHealth <= 0)
+        {
+            transform.SetPositionAndRotation(new Vector2(), new Quaternion());
+            playerHealth = 3;
+        }
+
+        Vector2 velocity = myRB.velocity;
+
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+        velocity.y = Input.GetAxisRaw("Vertical") * speed;
+
+        myRB.velocity = velocity;
+
+        if (canShoot)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x, transform.position.y + 1), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletSpeed);
+
+                mySpeaker.clip = shoot;
+                mySpeaker.play();
+
+                Destroy(b, bulletLifespan);
+                canShoot = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x, transform.position.y - 1), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -bulletSpeed);
+
+                mySpeaker.clip = shoot;
+                mySpeaker.play();
+
+                Destroy(b, bulletLifespan);
+                canShoot = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x - 1, transform.position.y), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(-bulletSpeed, 0);
+
+                mySpeaker.clip = shoot;
+                mySpeaker.play();
+
+                Destroy(b, bulletLifespan);
+                canShoot = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x + 1, transform.position.y), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+
+                mySpeaker.clip = shoot;
+                mySpeaker.play();
+
+                Destroy(b, bulletLifespan);
+                canShoot = false;
+            }
+        }
+
+        if(canShoot == false)
+        {
+            timeDifference += Time.deltaTime;
+
+            if (timeDifference >= shootCooldownTime)
+            {
+                canShoot = true;
+                timeDifference = 0;
+            }
+        }
+
+        if (invincible == true)
+        {
+            timeDifference += Time.deltaTime;
+
+            if (timeDifference2 >= invincibleCooldownTime)
+            {
+                invincible = false;
+                timeDifference = 0;
+                GetComponent<SpiritRenderer>().color = UnityEngine.color.white;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("enemy") && !invincible)
+        {
+            mySpeaker.clip = death;
+            mySpeaker.Play();
+            playerHealth--;
+        }
+
+        else if((collision.gameObject.name == "pickup"))
+        {
+            // This also means playerHealth = playerHealth + 1;
+            //playerHealth++;
+ 
+            mySpeaker.clip = pickup;
+            mySpeaker.Play();
+            invincible = true;
+            GetComponent<SpiritRenderer>().color = UnityEngine.Color.yellow;
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
